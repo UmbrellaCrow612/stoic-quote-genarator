a6abdd773451a31f50a7494cf58817879a33f99b
yousaf wazir <umbrellaman612@gmail.com>
2023-07-31 21:15:48 +0100

    added get all quotes

---
+++ src/pages/api/quotes/index.ts
@@ -0,0 +1,16 @@
+import { prisma } from "@/db";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET")
+    return res
+      .status(400)
+      .json({ message: `Method ${req.method} not allowed. Please use GET` });
+
+  const quotes = await prisma.quote.findMany();
+
+  res.status(200).json({ quotes });
+}
