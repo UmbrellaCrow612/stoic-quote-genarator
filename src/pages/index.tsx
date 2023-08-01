5b84ec5f7ccf7d0613822410215128b761a61555
yousaf wazir <umbrellaman612@gmail.com>
2023-08-01 15:07:55 +0100

    fixed env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,3 @@
-import React, { useState, useEffect } from "react";
-
-const Home = () => {
-  const [authors, setAuthors] = useState([]);
-
-  useEffect(() => {
-    const fetchAuthors = async () => {
-      try {
-        const response = await fetch("/api/authors");
-        const data = await response.json();
-        setAuthors(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchAuthors();
-  }, []);
-
-  return (
-    <div>
-      <h2>Authors</h2>
-      <pre>{JSON.stringify(authors, null, 2)}</pre>
-    </div>
-  );
-};
-
-export default Home;
+export default function Home() {
+  return <>hello</>;
+}
