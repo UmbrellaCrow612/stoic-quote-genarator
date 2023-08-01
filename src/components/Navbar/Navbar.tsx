2ad3d70a425456747b698d39ff39ab92752dd417
yousaf wazir <umbrellaman612@gmail.com>
2023-08-01 15:47:41 +0100

    added nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
 export default function Navbar() {
   return (
     <>
-      <nav>Hello nav</nav>
+      <nav className="flex items-center justify-between h-16 max-w-screen-xl px-4 mx-auto xl:px-0">
+        <Button variant={"ghost"} asChild className="uppercase">
+          <Link href="/">Stoicism</Link>
+        </Button>
+
+        <div className="flex gap-5">
+          <Button variant={"outline"} asChild className="">
+            <Link href="/">Stoics</Link>
+          </Button>
+          <Button variant={"outline"} asChild className="">
+            <Link href="/">Quotes</Link>
+          </Button>
+        </div>
+      </nav>
     </>
   );
 }
