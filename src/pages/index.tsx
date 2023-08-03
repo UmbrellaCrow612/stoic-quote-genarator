1cb500abf772a1931fffa3d644871133695f38a8
yousaf wazir <umbrellaman612@gmail.com>
2023-08-03 13:31:07 +0100

    added random quote section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <>hello</>;
+  return (
+    <section className="min-h-screen max-w-screen-xl mx-auto px-4 xl:px-0 flex items-center justify-center text-center flex-col gap-4">
+      {/* Animate in and fade in */}
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        &quot;Taxing Laughter: The Joke Tax Chronicles&quot;
+      </h1>
+      <p className="text-xl text-muted-foreground">- Marcus</p>
+    </section>
+  );
 }
