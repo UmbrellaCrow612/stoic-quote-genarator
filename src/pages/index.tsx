745cf76157477d46709eca71ca05c1ccafe7f315
yousaf wazir <umbrellaman612@gmail.com>
2023-08-09 11:53:31 +0100

    info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center max-w-screen-xl min-h-screen gap-4 px-4 mx-auto text-center xl:px-0">
       {/* Animate in and fade in */}
+      {/* This is fetching it every 3 seconds because of config, add revalidate or in out fade or show new quotes like a carousel*/}
+
       <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">
         {isLoading ? <>Loading</> : <>&quot;{data.text}&quot;</>}
       </h1>
