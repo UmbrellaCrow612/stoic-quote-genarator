2ac5264c2370bef2a62d48e9d34383c6da4157d3
yousaf wazir <umbrellaman612@gmail.com>
2023-08-09 11:34:31 +0100

    added single quote page

--- src/pages/quotes/[id].tsx
+++ src/pages/quotes/[id].tsx
@@ -1,3 +1,11 @@
 export default function Page() {
-    return <></>
+  return (
+    <section className="flex flex-col items-center justify-center max-w-screen-xl min-h-screen gap-4 px-4 mx-auto text-center xl:px-0">
+      {/* Animate in and fade in */}
+      <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">
+        &quot;Specific quote&quot;
+      </h1>
+      <p className="text-xl text-muted-foreground">- Marcus</p>
+    </section>
+  );
 }
